aac509510eaab199df0f38ce929d3d89812ea3d4
kev5s <kevin@curlybrackets.com.lb>
2019-11-24 18:48:25 +0200

    commit for section 4

--- backend/app.js
+++ backend/app.js
@@ -1,43 +1,71 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Post = require('./models/post');
 const app = express();
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb+srv://Kevin:sk3Hcs4winHmINo9@cluster0-u0pxi.mongodb.net/node-angular?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Connection to database success');
+  }).catch((err) => {
+    console.log(err, 'Connection failed');
+  })
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS"
-    );
+    'Access-Control-Allow-Origin', '*'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  );
+
   next();
 })
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post, 'post from post');
-  res.status(201).json({
-    message: "post added success"
-  })
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save().then((result) => {
+    console.log(result, 'resultsss');
+    res.status(201).json({
+      message: "post added success",
+      postId: result._id
+    })
+  });
 })
 
+
 app.get('/api/posts', (req, res, next) => {
-  const posts = [{
-      id: "fadsdada44",
-      title: "First serve post",
-      content: "server first content"
-    },
-    {
-      id: "kdkasdadss4",
-      title: "Second serve post",
-      content: "server second content"
-    }
-  ];
+  Post.find()
+    .then((documents) => {
+      res.status(200).json({
+        message: "Post success",
+        posts: documents
+      })
+    });
+})
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({
+    _id: req.params.id
+  }).then(result => {
+    console.log(result);
+  })
   res.status(200).json({
-    message: "Post success",
-    posts: posts
+    message: 'post deleted'
   });
 })
 
--- src/app/posts/posts-service.service.ts
+++ src/app/posts/posts-service.service.ts
@@ -15,11 +15,11 @@ export class PostsServiceService {
   ) { }
 
   getPosts() {
-    this.httpClient.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts').subscribe((postData) => {
-      console.log(postData, 'resources');
-      this.posts = postData.posts;
-      this.postUpdated.next([...this.posts]);
-    });
+    this.httpClient.get<{ message: string, posts: Post[] }>('http://localhost:3000/api/posts')
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postUpdated.next([...this.posts]);
+      });
   }
 
   getPostUpdateListener() {
@@ -27,11 +27,21 @@ export class PostsServiceService {
   }
 
   addPost(post: Post) {
-    this.httpClient.post<{message}>('http://localhost:3000/api/posts', post)
-      .subscribe((res) => {
-        console.log(res.message, 'response message');
+    this.httpClient.post<{ message: string, postId: string }>('http://localhost:3000/api/posts', post)
+      .subscribe((res) => {   
+        const id = res.postId;
+        post._id = id;
         this.posts.push(post);
         this.postUpdated.next([...this.posts]);
-      })
+      });
+  }
+
+  deletePost(postId: string) {
+    this.httpClient.delete('http://localhost:3000/api/posts/' + postId)
+    .subscribe(() => {
+      const updatedPosts = this.posts.filter(post => post._id != postId);
+      this.posts = updatedPosts;
+      this.postUpdated.next([...this.posts]);
+    });
   }
 }
